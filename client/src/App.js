2bd146320ad9196718f753d82c71689d1c7a36cf
timosha <tinaatimkovaa@gmail.com>
2022-03-03 22:43:40 -0500

    refactored code in App.js, using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Movies from './components/Movies.js';
 import axios from 'axios';
-import { Component, Fragment } from 'react';
+import { useState, Fragment } from 'react';
 import { Button } from 'react-bootstrap';
 
-class App extends Component {
-  state = {
-    movies: [],
-    loading: false
-  }
+const App = () => {
 
-  handleClick = (e) => {
-    this.setState({ loading: true });
+const [movies, setMovies] = useState([]);
+const [loading, setLoading] = useState(false);
+
+  const handleClick = (e) => {
+    setLoading(true);
     const id = e.target.id
 
     let url = (id === '1') ? 'http://www.omdbapi.com/?s=Matrix&apikey=720c3666'  :
@@ -19,26 +18,24 @@ class App extends Component {
                 'http://www.omdbapi.com/?s=Matrix%20Revolutions&apikey=720c3666';
 
     axios.get(url)
-    .then(res => this.setState({ movies: res.data.Search, loading: false }))
+    .then(res => { setMovies(res.data.Search); setLoading(false) })
 
-    axios.post('http://localhost:5000/movie', this.state.movies)
+    axios.post('http://localhost:5000/movie', movies)
     .then(response => console.log(response))
-    .then(() => axios.post('http://localhost:5000/poster', this.state.movies))
+    .then(() => axios.post('http://localhost:5000/poster', movies))
     .catch(error => console.log(error))
   }
 
-  render () {
     return (
       <Fragment>
         <div className='button-group'>
-        <Button variant='outline-primary' id='1' onClick={this.handleClick}>Search</Button>
-        <Button variant='outline-secondary' id='2' onClick={this.handleClick}>Search</Button>
-        <Button variant='outline-warning' id='3' onClick={this.handleClick}>Search</Button>
+        <Button variant='outline-primary' id='1' onClick={handleClick}>Search</Button>
+        <Button variant='outline-secondary' id='2' onClick={handleClick}>Search</Button>
+        <Button variant='outline-warning' id='3' onClick={handleClick}>Search</Button>
         </div>
-       <Movies loading={this.state.loading} movies={this.state.movies} />
+       <Movies loading={loading} movies={movies} />
       </Fragment>
     );
-  }
 }
 
 export default App;
